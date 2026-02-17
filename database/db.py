6f51febaa63d1850a92b2345792d17e5defb1914
Aniya-Roy <aniyaroy2306@gmail.com>
2026-02-18 03:48:13 +0530

    Update database connection configuration

--- database/db.py
+++ database/db.py
@@ -3,23 +3,18 @@ import mysql.connector
 from urllib.parse import urlparse
 
 def get_db_connection():
-    db_url = os.environ.get("mysql://root:dGLbvILUaIrkstjmUPvNfHQhvAeyzTUk@shortline.proxy.rlwy.net:23752/railway")
+    db_url = os.environ.get("mysql://root:dGLbvILUaIrkstjmUPvNfHQhvAeyzTUk@mysql.railway.internal:3306/railway")
 
-    if db_url:
-        parsed = urlparse(db_url)
+    # If this is missing, the app will stop and tell you why in the Render logs
+    if not db_url:
+        raise ConnectionError("Missing DATABASE_URL! Go to Render Dashboard -> Environment.")
 
-        return mysql.connector.connect(
-            host=parsed.hostname,
-            user=parsed.username,
-            password=parsed.password,
-            database=parsed.path.lstrip('/'),
-            port=parsed.port or 3306,
-            ssl_disabled=False
-        )
+    parsed = urlparse(db_url)
 
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="qr_ordering_db"
+        host=parsed.hostname,
+        user=parsed.username,
+        password=parsed.password,
+        database=parsed.path.lstrip('/'),
+        port=parsed.port or 3306
     )
